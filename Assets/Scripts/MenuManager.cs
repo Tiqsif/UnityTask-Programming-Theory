f95a78771dfdd3c287389e26d59754f100115b72
Tiqsif <yekurtarici@gmail.com>
2024-01-24 22:30:21 +0300

    added main menu
    - title
    - start button to change scene
    - exit button to exit the application

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
